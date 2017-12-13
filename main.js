8ed8095d1fce556c4c9a064ffab862c53f6a5e20
Dimitriy Kosianchuk <kosianchuk.d@gmail.com>
2017-12-13 04:42:36 +0200

    slider

---
+++ main.js
@@ -0,0 +1,22 @@
+var btnPrev = document.querySelector('.btn_prev');
+var btnNext = document.querySelector('.btn_next');
+var prev = 0;
+var polosa = document.getElementById('polosa');
+btnPrev.addEventListener('click', function(){
+  prev = prev + 800;
+  if ( prev  >= 0) {
+    prev =  -3200;
+    }
+  polosa.style.left = prev +"px";
+
+})
+
+btnNext.addEventListener('click', function(){
+  prev = prev - 800;
+  console.log(prev);
+  console.dir(polosa.offsetWidth + prev);
+  if ((polosa.offsetWidth + prev) < 800) {
+    prev = 0;
+  }
+  polosa.style.left = prev +"px";
+});
